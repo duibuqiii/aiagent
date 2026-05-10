614082574a838fd33736e17db571d09d24bd2c35
duibuqiii <with_me0607@163.com>
2026-05-10 16:49:40 +0800

    测试apikey的使用 openai库的基本使用

---
+++ 02OpenAI库的基本使用.py
@@ -0,0 +1,14 @@
+
+from openai import OpenAI
+client = OpenAI(
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+)
+response = client.chat.completions.create(
+    model="qwen3-max",
+    messages=[
+        {"role":"system","content":"你是一个python专家，并且不说废话简单回答！"},
+        {"role":"assistant","content":"好的，我是一个编程专家，并且不说废话！你要问什么？"},
+        {"role":"user","content":"输出1-10的数字,使用python"}
+    ]
+)
+print(response.choices[0].message.content)
